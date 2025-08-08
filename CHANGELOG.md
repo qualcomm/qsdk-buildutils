9941ea1caa72af0e1c49849a69807c04cbdc1854
Saahil Tomar <quic_saahtoma@quicinc.com>
2025-08-08 14:58:31 +0530

    Add setup.py, requirements.txt, and initial CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+<!-- markdownlint-disable MD024-->
+# **Change Log** 📜📝
+
+All notable changes to the "**Project template**" repository will be documented in this file.
+
+---
+## [**0.1.0**] - 2025-08-08
+
+### Added
+
+* Added a CHANGELOG.md.
+* Initial version of helper scripts.
+* Initial commit with file structure.
---
+++ setup.py
@@ -0,0 +1,16 @@
+# ===========================================================================
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+# ===========================================================================
+
+from setuptools import setup, find_packages
+
+setup(
+    name='meta_qsdk',
+    version='0.1.0',
+    author='Saahil',
+    description='Meta QSDK project for Qualcomm software development kit',
+    packages=find_packages(),
+    install_requires=[],
+)
+
