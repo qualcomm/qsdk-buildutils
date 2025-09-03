78009a0ade0cb9d9f4816b57a5f7a9dff9950f99
Saahil Tomar <quic_saahtoma@quicinc.com>
2025-09-03 14:23:44 +0530

    Updated License markings

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # ===========================================================================
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: BSD-3-Clause-Clear
 # ===========================================================================
 
 from setuptools import setup, find_packages
@@ -8,7 +8,6 @@ from setuptools import setup, find_packages
 setup(
     name='meta_qsdk',
     version='0.1.0',
-    author='Saahil',
     description='Meta QSDK project for Qualcomm software development kit',
     packages=find_packages(),
     install_requires=[],
