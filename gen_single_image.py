ed4a82a675d208edec35a31df04f3fee280c0b9e
Saahil Tomar <quic_saahtoma@quicinc.com>
2025-09-09 12:07:15 +0530

    Updated script gen_single_image.py
    Signed-off-by: Saahil Tomar <saahtoma@qti.qualcomm.com>

--- gen_single_image.py
+++ gen_single_image.py
@@ -18,6 +18,10 @@ def main(target, arch, folder_name):
 		os.makedirs(input_dir, exist_ok=True)
 		output_dir = os.path.join(input_dir, "bin")
 
+		img_file_path = os.path.join(input_dir, "WLAN_CALDB_qcn9224_v2_merged_compressed.img")
+		subprocess.run(f"touch {img_file_path}", shell=True, check=True)
+		subprocess.run(f"echo 'WLAN_CALDB_qcn9224_v2_merged_compressed.img' > {img_file_path}", shell=True, check=True)
+
 		if target == "ipq54xx" and arch == "32":
 			commands = [
 				f"python {input_dir}/prepareSingleImage.py --arch ipq5424 --fltype nand,emmc,norplusnand,norplusnand-gpt,norplusemmc,norplusemmc-gpt --in {input_dir} --gencdt --genxblcfg --genpart --genmbn",
@@ -47,36 +51,6 @@ def main(target, arch, folder_name):
 				f"python {input_dir}/scripts/pack_v3.py --arch ipq5424_64 --fltype emmc --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir} --flayout=vendor --img_suffix=vendor",
 				f"python {input_dir}/scripts/pack_v3.py --arch ipq5424_64 --fltype nand,emmc,norplusnand,norplusnand-gpt,norplusemmc,norplusemmc-gpt --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir} --image_type hlos",
 				f"python {input_dir}/scripts/pack_v3.py --arch ipq5424_64 --fltype emmc --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir} --flayout=vendor --img_suffix=vendor --image_type hlos"
-			]
-		elif target == "ipq53xx" and arch == "32":
-			commands = [
-				f"sed -i -- 's#wifi_fw_ipq5332_qcn9224_v2_qcn6432_qcn9160_squashfs.img#wifi_fw_ipq5332_qcn6432_squashfs.img#g' {input_dir}/ipq5332/config.xml",
-				f"sed -i -- 's#wifi_fw_ipq5332_qcn6432cs_qcn9160_squashfs.img#wifi_fw_ipq5332_qcn6432_squashfs.img#g' {input_dir}/ipq5332/config.xml",
-				f"sed -i -- 's#wifi_fw_ipq5332_qcn9224_v2_qcn9160_squashfs.img#wifi_fw_ipq5332_qcn9224_v2_single_dualmac_squashfs.img#g' {input_dir}/ipq5332/config.xml",
-				f"python {input_dir}/prepareSingleImage.py --arch ipq5332 --fltype emmc,nand,norplusemmc,norplusnand --in {input_dir}/ --gencdt --genmbn --genpart --genbootconf",
-				f"python {input_dir}/scripts/pack_v3.py --arch ipq5332 --fltype emmc,nand,norplusemmc,norplusnand --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir}",
-				f"python {input_dir}/scripts/pack_v3.py --arch ipq5332 --fltype emmc,nand,norplusemmc,norplusnand --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir} --image_type hlos"
-			]
-		elif target == "ipq53xx" and arch == "64":
-			commands = [
-				f"sed -i -- 's#wifi_fw_ipq5332_qcn9224_v2_qcn6432_qcn9160_squashfs.img#wifi_fw_ipq5332_qcn6432_squashfs.img#g' {input_dir}/ipq5332/config.xml",
-                f"sed -i -- 's#wifi_fw_ipq5332_qcn6432cs_qcn9160_squashfs.img#wifi_fw_ipq5332_qcn6432_squashfs.img#g' {input_dir}/ipq5332/config.xml",
-                f"sed -i -- 's#wifi_fw_ipq5332_qcn9224_v2_qcn9160_squashfs.img#wifi_fw_ipq5332_qcn9224_v2_single_dualmac_squashfs.img#g' {input_dir}/ipq5332/config.xml",
-				f"python {input_dir}/prepareSingleImage.py --arch ipq5332_64 --fltype emmc,nand,norplusemmc,norplusnand --in {input_dir}/ --gencdt --genmbn --genpart --genbootconf",
-				f"python {input_dir}/scripts/pack_v3.py --arch ipq5332_64 --fltype emmc,nand,norplusemmc,norplusnand --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir}",
-				f"python {input_dir}/scripts/pack_v3.py --arch ipq5332_64 --fltype emmc,nand,norplusemmc,norplusnand --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir} --image_type hlos"
-			]
-		elif target == "ipq95xx" and arch == "32":
-			commands = [
-				f"unsquashfs -d {input_dir}/tmp_final_v2_1 {input_dir}/wifi_fw_ipq9574_squashfs.img && unsquashfs -d {input_dir}/tmp_wkk_v2_1 {input_dir}/wifi_fw_qcn9224_v2_dualmac_squashfs.img",
-                f"cp -rf {input_dir}/tmp_wkk_v2_1/* {input_dir}/tmp_final_v2_1",
-                f"{input_dir}/mksquashfs4 {input_dir}/tmp_final_v2_1 {input_dir}/wifi_fw_ipq9574_qcn9224_v2_dualmac_squashfs.img -noappend -root-owned -comp xz -Xpreset 9 -Xe -Xlc 0 -Xlp 2 -Xpb 2 -Xbcj arm -b 256k -processors 1",
-                f"unsquashfs -d {input_dir}/tmp_final_v2_2 {input_dir}/wifi_fw_ipq9574_qcn9000_squashfs.img && unsquashfs -d {input_dir}/tmp_wkk_v2_2 {input_dir}/wifi_fw_qcn9224_v2_squashfs.img",
-                f"cp -rf {input_dir}/tmp_wkk_v2_2/* {input_dir}/tmp_final_v2_2",
-                f"{input_dir}/mksquashfs4 {input_dir}/tmp_final_v2_2 {input_dir}/wifi_fw_ipq9574_qcn9000_qcn9224_v2_squashfs.img -noappend -root-owned -comp xz -Xpreset 9 -Xe -Xlc 0 -Xlp 2 -Xpb 2 -Xbcj arm -b 256k -processors 1",
-				f"python {input_dir}/prepareSingleImage.py --arch ipq9574 --fltype nand,norplusnand,emmc,norplusemmc --in {input_dir} --gencdt --genbootconf --genpart --genmbn",
-				f"python {input_dir}/scripts/pack.py --arch ipq9574 --fltype nand,norplusnand,emmc,norplusemmc  --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir} --multi_wifi_fw",
-				f"python {input_dir}/scripts/pack.py --arch ipq9574 --fltype nand,norplusnand,emmc,norplusemmc  --srcPath {input_dir} --inImage {input_dir} --outImage {output_dir} --image_type hlos --multi_wifi_fw"
 			]
 		elif target == "ipq95xx" and arch == "64":
 			commands = [
